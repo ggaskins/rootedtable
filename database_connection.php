91329fd3054d80059c581eea41c1d37d6d832055
ggaskins <122938763+ggaskins@users.noreply.github.com>
2023-04-17 19:01:03 -0400

    Update database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -1,4 +1,4 @@
 <?php
 //database_connection.php
-$connect = new PDO("mysql:host=phpstack-976529-3418661.cloudwaysapps.com;port=8082;dbname=uvjtxphbtk", "uvjtxphbtk", "pktRyT7DW8");
+$connect = new PDO("mysql:host=https://phpstack-983720-3449172.cloudwaysapps.com;port=8082;dbname=uvjtxphbtk", "uvjtxphbtk", "pktRyT7DW8");
 ?>
