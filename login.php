5b00e96866777662afa1efcfea22bb8b4506d867
ggaskins <122938763+ggaskins@users.noreply.github.com>
2023-04-19 12:12:45 -0400

    added basic animation

--- login.php
+++ login.php
@@ -50,10 +50,87 @@
     .submit-button:hover {
       background-color: #0077A7;
     }
+    
+    .scene {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+      z-index: -1;
+    }
+
+    .farmer,
+    .cloud {
+      position: absolute;
+      bottom: 10%;
+    }
+
+    .farmer {
+      left: 10%;
+      width: 30px;
+      height: 60px;
+      background-color: #000;
+      animation: farmerAnimation 2s infinite alternate;
+    }
+
+    .farmer::before {
+      content: "";
+      position: absolute;
+      top: -20px;
+      left: 50%;
+      margin-left: -10px;
+      width: 20px;
+      height: 20px;
+      background-color: #000;
+      border-radius: 50%;
+    }
+
+    .cloud {
+      left: 0;
+      width: 60px;
+      height: 30px;
+      background-color: #fff;
+      border-radius: 50%;
+      opacity: 0.8;
+      animation: cloudAnimation 10s linear infinite;
+    }
+
+    .cloud::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 20px;
+      width: 40px;
+      height: 40px;
+      background-color: #fff;
+      border-radius: 50%;
+    }
+
+    @keyframes farmerAnimation {
+      0% {
+        transform: translateY(0);
+      }
+      100% {
+        transform: translateY(-10px);
+      }
+    }
+
+    @keyframes cloudAnimation {
+      0% {
+        transform: translateX(-100%);
+      }
+      100% {
+        transform: translateX(100%);
+      }
+    }
   </style>
 </head>
 <body>
   <div class="login-container">
+    <div class="scene">
+      <div class="farmer"></div>
+      <div class="cloud"></div>
+    </div>
     <form class="login-form" action="authenticate.php" method="post">
       <h2>Login</h2>
       <input type="email" name="email" class="form-control" placeholder="Email" required>
