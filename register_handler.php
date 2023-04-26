310ea5e26673a56268a449f6ea816b815465b334
ggaskins <122938763+ggaskins@users.noreply.github.com>
2023-04-26 14:33:32 -0400

    Update register_handler.php

--- register_handler.php
+++ register_handler.php
@@ -6,7 +6,7 @@ $db_user = 'doadmin';
 $db_pass = 'AVNS_TiObYQOBYOU5Klx6sf8 hide';
 
 // Create a database connection
-$link = mysqli_connect($db_host, $db_user, $db_pass, $db_name, 25060, array('sslmode' => 'REQUIRED'));
+$link = mysqli_connect($db_host . ':' . $db_port, $db_user, $db_password, $db_name);
 
 // Check connection
 if (!$link) {
