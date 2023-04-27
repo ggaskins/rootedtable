b5e0607a667d1f1e3c2ee519e3e65fc0d12bc4f5
ggaskins <122938763+ggaskins@users.noreply.github.com>
2023-04-27 08:00:20 -0400

    Update account.php

--- account.php
+++ account.php
@@ -2,6 +2,8 @@
 // Initialize session
 session_start();
 
+// Include header file
+require_once "header.php";
 // Check if the user is logged in, if not then redirect to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
   header("location: login.php");
