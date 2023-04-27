7c2f2382b6679aa509796c1b88a18b9534791831
ggaskins <122938763+ggaskins@users.noreply.github.com>
2023-04-26 20:37:55 -0400

    Update local_farms.php

--- local_farms.php
+++ local_farms.php
@@ -63,9 +63,10 @@ require_once "header.php";
 
 // Database configuration
 $db_host = 'db-mysql-nyc1-74817-do-user-13891110-0.b.db.ondigitalocean.com';
+$db_port = '25060';
 $db_name = 'defaultdb';
 $db_user = 'doadmin';
-$db_pass = 'AVNS_TiObYQOBYOU5Klx6sf8 hide';
+$db_password = 'AVNS_TiObYQOBYOU5Klx6sf8';
 
 // Create a database connection
 $conn = mysqli_connect($db_host . ':' . $db_port, $db_user, $db_password, $db_name);
