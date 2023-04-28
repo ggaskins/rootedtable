73cfd2781f569d0e23e643322a6e2d0e281f3c51
ggaskins <122938763+ggaskins@users.noreply.github.com>
2023-04-28 09:02:03 -0400

    Update login.php

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
   // Start session
   session_start();
-
+  ini_set('session.cookie_secure', 1);
   // Check if user is already logged in, if yes then redirect to welcome page
   if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
     header("location: welcome.php");
